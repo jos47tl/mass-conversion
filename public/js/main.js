11f920828f839fa40ec11cdae3c31b980cf29e8c
Josh Li <josli@pdx.edu>
2024-09-14 19:37:44 -0700

    Add javascript to populate select dropdowns instead of hardcoding into html

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,33 @@
 const convertUrl = "http://localhost:3000/api/convert";
 const combineUrl = "http://localhost:3000/api/combine";
 
-let convertValue = document.getElementById("convertValue");
-let fromUnit = document.getElementById("fromUnit");
-let toUnit = document.getElementById("toUnit");
-let convertButton = document.getElementById("convertButton");
-let resultCard = document.getElementById("resultCard");
+const convertValue = document.getElementById("convertValue");
+const fromUnit = document.getElementById("fromUnit");
+const toUnit = document.getElementById("toUnit");
+const convertButton = document.getElementById("convertButton");
+const combineButton = document.getElementById("combineButton");
+const convertResult = document.getElementById("convertResult");
+const selectDropdowns = document.getElementsByTagName("select");
+
+const options = [
+  { value: "kg", text: "Kilogram (kg)" },
+  { value: "g", text: "Gram (g)" },
+  { value: "lb", text: "Pound (lb)" },
+  { value: "oz", text: "Ounce (oz)" },
+  { value: "t", text: "Metric ton (t)" },
+  { value: "short_t", text: "Short ton (US ton)" },
+  { value: "long_t", text: "Long ton (UK ton)" },
+];
+
+for (let i = 0; i < selectDropdowns.length; i++) {
+  const select = selectDropdowns[i];
+  options.forEach((option) => {
+    const optionElement = document.createElement("option");
+    optionElement.value = option.value;
+    optionElement.text = option.text;
+    select.appendChild(optionElement);
+  });
+}
 
 convertButton.addEventListener("click", () => {
   const value = convertValue.value;
