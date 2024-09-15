ff9c547a5f0b472b01bdb71cfd8330321824c00e
Josh Li <josli@pdx.edu>
2024-09-15 11:46:02 -0700

    Test single mass conversion

--- tests/convert.test.js
+++ tests/convert.test.js
@@ -24,5 +24,18 @@ describe("convertMass", () => {
   it("converting the same unit", () => {
     expect(convertMass(1, "kg", "kg")).toBe(1);
     expect(convertMass(0, "lb", "lb")).toBe(0);
+    expect(convertMass(-1, "long_t", "long_t")).toBe(-1);
   });
+  it("simple conversions", () => {
+    expect(convertMass(2, "kg", "lb")).toBe(4.409245244);
+    expect(convertMass(4.409245244, "lb", "kg")).toBe(2);
+  });
+  it("scientific notation", () => {
+    expect(convertMass(100, "t", "mg")).toBe("1e+11");
+    expect(convertMass(0.000001, "oz", "short_t")).toBe("3.125016328e-11");
+  });
+  it("edge cases", () => {
+    expect(convertMass("", "kg", "lb")).toBe(0);
+    expect(formatNumber("a")).toBeNaN();
+  })
 })
